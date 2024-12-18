4e45c19155afe71583aa97073785e1d3be24232c
Dana Luther <dana.luther@gmail.com>
2024-12-18 11:49:22 -0500

    PHP 8.4 Support Enabled
    Signed-off-by: Dana Luther <dluther@envisageinternational.com>

--- tests/codeception/bin/_bootstrap.php
+++ tests/codeception/bin/_bootstrap.php
@@ -7,4 +7,4 @@ defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', '/var/www/html/tests
 require('/var/www/html/vendor/autoload.php');
 require('/var/www/html/vendor/yiisoft/yii2/Yii.php');
 
-Yii::setAlias('@tests', dirname(dirname(__DIR__)));
+Yii::setAlias('@tests', dirname(__DIR__, 2));
